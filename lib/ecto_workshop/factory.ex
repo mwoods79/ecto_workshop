70625bded4e599fb027456f69151cd76e5833634
Micah Woods <micahwoods@gmail.com>
2019-06-13 10:16:16 -0400

    Add Factory for recipes

--- lib/ecto_workshop/factory.ex
+++ lib/ecto_workshop/factory.ex
@@ -1,3 +1,15 @@
 defmodule EctoWorkshop.Factory do
   use ExMachina.Ecto, repo: EctoWorkshop.Repo
+
+  alias EctoWorkshop.Menu.Recipe
+
+  def recipe_factory do
+    %Recipe{
+      title: Faker.Food.dish(),
+      description: Faker.Food.description(),
+      calories: Faker.Random.Elixir.random_between(150, 2500),
+      cook_time: Faker.Random.Elixir.random_between(10, 45),
+      prep_time: Faker.Random.Elixir.random_between(10, 45)
+    }
+  end
 end
